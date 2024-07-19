078285de364e1c9181715b3e1396e752e1569d30
SmyElysia <1254099253@qq.com>
2024-07-19 14:15:02 +0800

    1.1.1

--- start.sh
+++ start.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-#执行程序位置以及生成的日志文件位置
+# 执行程序位置以及生成的日志文件位置
 APP_NAME=Ant-0.0.1-SNAPSHOT.jar
 LOG_DIR=/root/trap/logs
-LOG_FILE=${LOG_DIR}/ant.log
 
-#使用说明，用来提示输入参数
+# 根据当前日期生成日志文件名
+DATE=$(date '+%Y-%m-%d')
+LOG_FILE=${LOG_DIR}/ant_${DATE}.log
+
+# 使用说明，用来提示输入参数
 usage() {
   echo "Usage: sh demo.sh [start|stop|restart|status]"
   exit 1
 }
 
-#检查程序是否在运行
+# 检查程序是否在运行
 is_exist() {
   pid=$(ps -ef | grep $APP_NAME | grep -v grep | awk '{print $2}')
-  #如果不存在返回1，存在返回0
+  # 如果不存在返回1，存在返回0
   if [ -z "${pid}" ]; then
     return 1
   else
@@ -21,18 +24,20 @@ is_exist() {
   fi
 }
 
-#启动方法，添加时间戳
+# 启动方法，添加时间戳
 start() {
   is_exist
   if [ $? -eq "0" ]; then
     echo "${APP_NAME} is already running. pid=${pid} ."
   else
-    nohup java -jar ${APP_NAME} --server.port=8082 -Xmx512m -Xms512m -Xmn256m >> ${LOG_FILE} 2>&1 &  #分配Jvm以及服务端口号
+    # 使用当前日期生成新的日志文件
+    LOG_FILE=${LOG_DIR}/ant_$(date '+%Y-%m-%d').log
+    nohup java -jar ${APP_NAME} --server.port=8082 -Xmx512m -Xms512m -Xmn256m >> ${LOG_FILE} 2>&1 &  # 分配Jvm以及服务端口号
     echo "$(date '+%Y-%m-%d %H:%M:%S') - ${APP_NAME} started." >> ${LOG_FILE}
   fi
 }
 
-#停止方法
+# 停止方法
 stop() {
   is_exist
   if [ $? -eq "0" ]; then
@@ -43,7 +48,7 @@ stop() {
   fi
 }
 
-#输出运行状态
+# 输出运行状态
 status() {
   is_exist
   if [ $? -eq "0" ]; then
@@ -52,13 +57,14 @@ status() {
     echo "${APP_NAME} is not running."
   fi
 }
-#重启
+
+# 重启
 restart() {
   stop
   start
 }
 
-#根据输入参数，选择执行对应方法，不输入则执行使用说明
+# 根据输入参数，选择执行对应方法，不输入则执行使用说明
 case "$1" in
 "start")
   start
